4f5a8a5e48d36295e455f9bdbd5ed48df4b7f83d
Nayak29 <nayakshivajee@gmail.com>
2024-04-26 11:24:51 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 75px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b><center>COPYRIGHT ©2024 Developed by Nurul Huq, Prabin Kumar Mishra, Shivajee Nayak.<br>
+  Donate Blood
+  <br>
+  ALL RIGHTS RESERVED.
+  </center>
+  </div>
+
+
+</body>
+
+</html>
